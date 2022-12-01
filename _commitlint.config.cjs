d8068bbc78bfd26220089deca090162d333e0571
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2022-12-02 08:53:53 +0900

    chore: 🏡 remove commitlint.cjs

--- _commitlint.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("@commitlint/types").UserConfig} */
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  plugins: ["commitlint-plugin-empty-issues-rule"],
-  rules: {
-    "empty-issues": [1, "always"],
-  },
-};
